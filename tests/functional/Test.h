9820b05d60a02ea7397aea34db2f9b4fb1f76dd9
Kiril Dichev <kiril.dichev@huawei.com>
2024-08-30 10:54:49 +0200

    In a middle of a big mess of changes, which I hope will end well

--- tests/functional/Test.h
+++ tests/functional/Test.h
@@ -22,84 +22,86 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <gtest/gtest.h>
+
 #include "assert.hpp"
 
-#define EXPECT_EQ( format, expected, actual ) \
-    do { LPFLIB_IGNORE_TAUTOLOGIES  \
-        if ( (expected) != (actual) ) { \
-        fprintf(stderr, "TEST FAILURE in " __FILE__ ":%d\n" \
-                  "   Expected (%s) which evaluates to " format ", but\n" \
-                  "   actual (%s) which evaluates to " format ".\n",  __LINE__, \
-                  #expected, (expected), #actual, (actual) ); \
-        abort(); } \
-        LPFLIB_RESTORE_WARNINGS } while(0)
-
-#define EXPECT_NE( format, expected, actual ) \
-    do { LPFLIB_IGNORE_TAUTOLOGIES  \
-        if ( (expected) == (actual) ) { \
-        fprintf(stderr, "TEST FAILURE in " __FILE__ ":%d\n" \
-                  "   Expected (%s) which evaluates to " format " to be different from\n" \
-                  "   actual (%s) which evaluates to " format ".\n",  __LINE__, \
-                  #expected, (expected), #actual, (actual) ); \
-        abort(); } \
-        LPFLIB_RESTORE_WARNINGS } while(0)
-
-#define EXPECT_LE( format, expected, actual ) \
-    do { LPFLIB_IGNORE_TAUTOLOGIES  \
-        if ( !( (expected) <= (actual)) ) { \
-        fprintf(stderr, "TEST FAILURE in " __FILE__ ":%d\n" \
-                  "   Expected that (%s) which evaluates to " format ", is less than\n" \
-                  "   or equal to (%s) which evaluates to " format ".\n",  __LINE__, \
-                  #expected, (expected), #actual, (actual) ); \
-        abort(); } \
-        LPFLIB_RESTORE_WARNINGS } while(0)
-
-#define EXPECT_GE( format, expected, actual ) \
-    do { LPFLIB_IGNORE_TAUTOLOGIES  \
-        if ( !( (expected) >= (actual) )) { \
-        fprintf(stderr, "TEST FAILURE in " __FILE__ ":%d\n" \
-                  "   Expected that (%s) which evaluates to " format ", is greater than\n" \
-                  "   or equal to (%s) which evaluates to " format ".\n",  __LINE__, \
-                  #expected, (expected), #actual, (actual) ); \
-        abort(); } \
-        LPFLIB_RESTORE_WARNINGS } while(0)
-
-#define EXPECT_LT( format, expected, actual ) \
-    do { LPFLIB_IGNORE_TAUTOLOGIES  \
-        if ( !( (expected) < (actual)) ) { \
-        fprintf(stderr, "TEST FAILURE in " __FILE__ ":%d\n" \
-                  "   Expected that (%s) which evaluates to " format ", is less than\n" \
-                  "   (%s) which evaluates to " format ".\n",  __LINE__, \
-                  #expected, (expected), #actual, (actual) ); \
-        abort(); } \
-        LPFLIB_RESTORE_WARNINGS } while(0)
-
-#define EXPECT_GT( format, expected, actual ) \
-    do { LPFLIB_IGNORE_TAUTOLOGIES  \
-        if ( !( (expected) > (actual) )) { \
-        fprintf(stderr, "TEST FAILURE in " __FILE__ ":%d\n" \
-                  "   Expected that (%s) which evaluates to " format ", is greater than\n" \
-                  "   (%s) which evaluates to " format ".\n",  __LINE__, \
-                  #expected, (expected), #actual, (actual) ); \
-        abort(); } \
-        LPFLIB_RESTORE_WARNINGS } while(0)
-
-#define EXPECT_STREQ( N, expected, actual ) \
-    do { LPFLIB_IGNORE_TAUTOLOGIES  \
-        if ( strncmp( (expected), (actual), (N) ) != 0 ) { \
-        fprintf(stderr, "TEST FAILURE in " __FILE__ ":%d\n" \
-                  "   Expected (%s) which evaluates to %s, but\n" \
-                  "   actual (%s) which evaluates to %s.\n",  __LINE__, \
-                  #expected, (expected), #actual, (actual) ); \
-        abort(); } \
-        LPFLIB_RESTORE_WARNINGS } while(0)
-
-#ifdef __GNUC__
-    #define UNUSED __attribute__((unused))
-#else
-    #define UNUSED
-#endif
-
+//#define EXPECT_EQ( format, expected, actual ) \
+//    do { LPFLIB_IGNORE_TAUTOLOGIES  \
+//        if ( (expected) != (actual) ) { \
+//        fprintf(stderr, "TEST FAILURE in " __FILE__ ":%d\n" \
+//                  "   Expected (%s) which evaluates to " format ", but\n" \
+//                  "   actual (%s) which evaluates to " format ".\n",  __LINE__, \
+//                  #expected, (expected), #actual, (actual) ); \
+//        abort(); } \
+//        LPFLIB_RESTORE_WARNINGS } while(0)
+//
+//#define EXPECT_NE( format, expected, actual ) \
+//    do { LPFLIB_IGNORE_TAUTOLOGIES  \
+//        if ( (expected) == (actual) ) { \
+//        fprintf(stderr, "TEST FAILURE in " __FILE__ ":%d\n" \
+//                  "   Expected (%s) which evaluates to " format " to be different from\n" \
+//                  "   actual (%s) which evaluates to " format ".\n",  __LINE__, \
+//                  #expected, (expected), #actual, (actual) ); \
+//        abort(); } \
+//        LPFLIB_RESTORE_WARNINGS } while(0)
+//
+//#define EXPECT_LE( format, expected, actual ) \
+//    do { LPFLIB_IGNORE_TAUTOLOGIES  \
+//        if ( !( (expected) <= (actual)) ) { \
+//        fprintf(stderr, "TEST FAILURE in " __FILE__ ":%d\n" \
+//                  "   Expected that (%s) which evaluates to " format ", is less than\n" \
+//                  "   or equal to (%s) which evaluates to " format ".\n",  __LINE__, \
+//                  #expected, (expected), #actual, (actual) ); \
+//        abort(); } \
+//        LPFLIB_RESTORE_WARNINGS } while(0)
+//
+//#define EXPECT_GE( format, expected, actual ) \
+//    do { LPFLIB_IGNORE_TAUTOLOGIES  \
+//        if ( !( (expected) >= (actual) )) { \
+//        fprintf(stderr, "TEST FAILURE in " __FILE__ ":%d\n" \
+//                  "   Expected that (%s) which evaluates to " format ", is greater than\n" \
+//                  "   or equal to (%s) which evaluates to " format ".\n",  __LINE__, \
+//                  #expected, (expected), #actual, (actual) ); \
+//        abort(); } \
+//        LPFLIB_RESTORE_WARNINGS } while(0)
+//
+//#define EXPECT_LT( format, expected, actual ) \
+//    do { LPFLIB_IGNORE_TAUTOLOGIES  \
+//        if ( !( (expected) < (actual)) ) { \
+//        fprintf(stderr, "TEST FAILURE in " __FILE__ ":%d\n" \
+//                  "   Expected that (%s) which evaluates to " format ", is less than\n" \
+//                  "   (%s) which evaluates to " format ".\n",  __LINE__, \
+//                  #expected, (expected), #actual, (actual) ); \
+//        abort(); } \
+//        LPFLIB_RESTORE_WARNINGS } while(0)
+//
+//#define EXPECT_GT( format, expected, actual ) \
+//    do { LPFLIB_IGNORE_TAUTOLOGIES  \
+//        if ( !( (expected) > (actual) )) { \
+//        fprintf(stderr, "TEST FAILURE in " __FILE__ ":%d\n" \
+//                  "   Expected that (%s) which evaluates to " format ", is greater than\n" \
+//                  "   (%s) which evaluates to " format ".\n",  __LINE__, \
+//                  #expected, (expected), #actual, (actual) ); \
+//        abort(); } \
+//        LPFLIB_RESTORE_WARNINGS } while(0)
+//
+//#define EXPECT_STREQ( N, expected, actual ) \
+//    do { LPFLIB_IGNORE_TAUTOLOGIES  \
+//        if ( strncmp( (expected), (actual), (N) ) != 0 ) { \
+//        fprintf(stderr, "TEST FAILURE in " __FILE__ ":%d\n" \
+//                  "   Expected (%s) which evaluates to %s, but\n" \
+//                  "   actual (%s) which evaluates to %s.\n",  __LINE__, \
+//                  #expected, (expected), #actual, (actual) ); \
+//        abort(); } \
+//        LPFLIB_RESTORE_WARNINGS } while(0)
+//
+//#ifdef __GNUC__
+//    #define UNUSED __attribute__((unused))
+//#else
+//    #define UNUSED
+//#endif
+//
 /** \mainpage Test documentation
  *
  * This documentation lists the tests of the LPF implementation
@@ -111,25 +113,25 @@
   * A set of small programs to test the LPF API.
   */
 
-#ifdef DOXYGEN
-#define TEST( name )                  \
-    /** \ingroup APITests        */  \
-    int name( lpf_pid_t P )   
-
-#else
-
-#define TEST( name )                  \
-    /** \ingroup APITests        */  \
-    int name(int argc, char ** argv); \
-                                      \
-    int main(int argc, char ** argv)  \
-    {                                 \
-        (void) argc; (void) argv;     \
-        return name (argc, argv);     \
-    }                                 \
-                                      \
-    int name(int argc UNUSED, char ** argv UNUSED)  
-
-#endif
+//#ifdef DOXYGEN
+//#define TEST( name )                  \
+//    /** \ingroup APITests        */  \
+//    int name( lpf_pid_t P )   
+//
+//#else
+//
+//#define TEST( name )                  \
+//    /** \ingroup APITests        */  \
+//    int name(int argc, char ** argv); \
+//                                      \
+//    int main(int argc, char ** argv)  \
+//    {                                 \
+//        (void) argc; (void) argv;     \
+//        return name (argc, argv);     \
+//    }                                 \
+//                                      \
+//    int name(int argc UNUSED, char ** argv UNUSED)  
+//
+//#endif
 
 #endif
