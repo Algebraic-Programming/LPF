77a31349843d9aa8a5023a4cb4d97374a857d80a
Kiril Dichev <kiril.dichev@huawei.com>
2024-09-06 18:38:14 +0200

    Slow progress, now I need to implement in my Python script the different logic if the bloody Gtest wants to just list the tests or run them

--- build-arm/death_test_launcher.py
+++ build-arm/death_test_launcher.py
@@ -3,18 +3,18 @@ import subprocess
 import sys
 
 parser = argparse.ArgumentParser( description='Death test launcher' )
-parser.add_argument( 'parallel_launcher', type=str)
-parser.add_argument( 'process_count', type=int)
-parser.add_argument( 'executable', type=str)
-parser.add_argument( 'expected_return_code', type=int)
+parser.add_argument("-L", "--parallel_launcher", type=str)
+parser.add_argument("-P", "--process_count", type=int)
+parser.add_argument("-R", "--expected_return_code", type=int)
+parser.add_argument( 'cmd', nargs=argparse.REMAINDER )
 args = parser.parse_args()
-run_cmd = [args.parallel_launcher, '-engine', 'ibverbs', '-n', str(args.process_count), args.executable]
+run_cmd = [args.parallel_launcher, '-engine', 'ibverbs', '-n', str(args.process_count), args.cmd[0], args.cmd[1]]
 print("Death test launcher command:")
 print(run_cmd)
 cmd = subprocess.run( run_cmd, stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL )
 retcode = cmd.returncode
 
 if (retcode != args.expected_return_code):
-    print("Test " + args.executable + "\nreturned\t" + str(retcode) + "\nexpected return code was: " + str(args.expected_return_code))
+    print("Test " + args.cmd[0] + args.cmd[1] + "\nreturned\t" + str(retcode) + "\nexpected return code was: " + str(args.expected_return_code))
     sys.exit(1)
-print("Test " + args.executable + " passed")
+print("Test " + args.cmd[0] + args.cmd[1] + " passed")
