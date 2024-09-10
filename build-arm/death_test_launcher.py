80f0392edaa30bb0197598bd9dc5ea6cbb0640d5
Kiril Dichev <kiril.dichev@huawei.com>
2024-09-10 09:41:23 +0200

    Got IB Verbs tests to work again by setting LPF_MPI_AUTO_INITIALIZE=0. Also, using Setup and TearDown for entire test suite now, pretty neat, no code duplication each time

--- build-arm/death_test_launcher.py
+++ build-arm/death_test_launcher.py
@@ -4,20 +4,27 @@ import sys
 
 parser = argparse.ArgumentParser( description='Death test launcher' )
 parser.add_argument("-L", "--parallel_launcher", type=str)
-parser.add_argument("-P", "--process_count", type=int)
+parser.add_argument("-p", "--min_process_count", type=int)
+parser.add_argument("-P", "--max_process_count", type=int)
+parser.add_argument("-t", "--lpf_probe_timer", type=float)
 parser.add_argument("-R", "--expected_return_code", type=int)
 parser.add_argument( 'cmd', nargs=argparse.REMAINDER )
 args = parser.parse_args()
-run_cmd = [args.parallel_launcher, '-engine', 'ibverbs', '-n', str(args.process_count), args.cmd[0], args.cmd[1]]
-print("Death test launcher command:")
-print(run_cmd)
-cmd = subprocess.run( run_cmd, capture_output=True)
-if args.cmd[1] != "--gtest_list_tests":
-    print("args command is " + args.cmd[1])
-    
-    retcode = cmd.returncode
-    
-    if (retcode != args.expected_return_code):
-        print("Test " + args.cmd[0] + args.cmd[1] + "\nreturned\t" + str(retcode) + "\nexpected return code was: " + str(args.expected_return_code))
-        sys.exit(1)
-print("Test " + args.cmd[0] + args.cmd[1] + " passed")
+if args.cmd[1] == "--gtest_list_tests":
+    run_cmd = [args.cmd[0], args.cmd[1]]
+    cmd = subprocess.run( run_cmd, capture_output=True)
+else:
+    for i in range(args.min_process_count, args.max_process_count+1):
+        if args.lpf_probe_timer > 0.0:
+            run_cmd = [args.parallel_launcher, '-engine', 'ibverbs', '-probe', args.lpf_probe_timer, '-n', str(i)] + args.cmd
+        else:
+            run_cmd = [args.parallel_launcher, '-engine', 'ibverbs', '-n', str(i)] + args.cmd
+        print("Run command: ")
+        print(run_cmd)
+        cmd = subprocess.run( run_cmd, capture_output=True)
+        print("Done with this, returned code = " + str(cmd.returncode))
+        retcode = cmd.returncode
+        if (retcode != args.expected_return_code):
+            print("Test " + args.cmd[0] + args.cmd[1] + "\nreturned\t" + str(retcode) + "\nexpected return code was: " + str(args.expected_return_code))
+            sys.exit(1)
+    print("Test " + args.cmd[0] + args.cmd[1] + " passed")
