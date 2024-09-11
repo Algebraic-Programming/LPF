703fde714598a862e549a2f5cd8be6d11bf1a316
Kiril Dichev <kiril.dichev@huawei.com>
2024-09-11 09:31:26 +0200

    First step towards making it work for many engines

--- test_launcher.py
+++ test_launcher.py
@@ -3,6 +3,7 @@ import subprocess
 import sys
 
 parser = argparse.ArgumentParser( description='Death test launcher' )
+parser.add_argument("-e", "--engine", type=str)
 parser.add_argument("-L", "--parallel_launcher", type=str)
 parser.add_argument("-p", "--min_process_count", type=int)
 parser.add_argument("-P", "--max_process_count", type=int)
@@ -16,13 +17,13 @@ if args.cmd[1] == "--gtest_list_tests":
 else:
     for i in range(args.min_process_count, args.max_process_count+1):
         if args.lpf_probe_timer > 0.0:
-            run_cmd = [args.parallel_launcher, '-engine', 'ibverbs', '-probe', str(args.lpf_probe_timer), '-n', str(i)] + args.cmd
+            run_cmd = [args.parallel_launcher, '-engine', args.engine, '-probe', str(args.lpf_probe_timer), '-n', str(i)] + args.cmd
         else:
-            run_cmd = [args.parallel_launcher, '-engine', 'ibverbs', '-n', str(i)] + args.cmd
+            run_cmd = [args.parallel_launcher, '-engine', args.engine, '-n', str(i)] + args.cmd
         print("Run command: ")
         print(run_cmd)
         cmd = subprocess.run( run_cmd, capture_output=True)
-        print("Done with this, returned code = " + str(cmd.returncode))
+        print("Test returned code = " + str(cmd.returncode))
         retcode = cmd.returncode
         if (retcode != args.expected_return_code):
             print("Test " + args.cmd[0] + args.cmd[1] + "\nreturned\t" + str(retcode) + "\nexpected return code was: " + str(args.expected_return_code))
