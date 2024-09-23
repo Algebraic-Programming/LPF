bef255ec23e7d37b3adb3459f2c905d7f8e84904
Kiril Dichev <kiril.dichev@huawei.com>
2024-09-23 14:31:13 +0200

    Typos and cleanup

--- .build-tools/slurm/build-job-arm.sh
+++ .build-tools/slurm/build-job-arm.sh
@@ -3,14 +3,15 @@
 #SBATCH --tasks-per-node 56
 #SBATCH --cpus-per-task=1
 #SBATCH --mem 0
+#SBATCH -t 03:00:00
 #SBATCH -o job-arm-%j.stdout
 #SBATCH -e job-arm-%j.stderr
 source $HOME/spack/share/spack/setup-env.sh
 spack env activate arm 
 HASH=$(git rev-parse --verify HEAD)
 echo $HASH
-mkdir build-arm
-../bootstraph.sh --functests 
+mkdir build-arm ; cd build-arm
+../bootstrap.sh --functests 
 make -j56
 ctest
 
