59873b9a7c1a510bfae2624da50892d0e8ad1d07
Kiril Dichev <kiril.dichev@huawei.com>
2024-09-26 15:13:01 +0000

    Use gitlab runner working dirs

--- .build-tools/reframe/get_and_build.sh
+++ .build-tools/reframe/get_and_build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-rm -rf /storage/distscratch/okorak/lpf_repo
-git clone --branch ci https://oath2:glpat-yqiQ3S1Emax8EoN91ycU@gitlab.huaweirc.ch/zrc-von-neumann-lab/spatial-computing/lpf/ /storage/distscratch/okorak/lpf_repo
-pushd /storage/distscratch/okorak/lpf_repo
+rm -rf /storage/users/gitlab-runner/lpf_repo
+git clone --branch ci https://oath2:glpat-yqiQ3S1Emax8EoN91ycU@gitlab.huaweirc.ch/zrc-von-neumann-lab/spatial-computing/lpf/ /storage/users/gitlab-runner/lpf_repo
+pushd /storage/users/gitlab-runner/lpf_repo
 mkdir build
 pushd build
 ../bootstrap.sh --functests; make -j32
