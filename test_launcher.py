7fecf7c8585c84763f3cb031854f2c8ea9457031
Kiril Dichev <kiril.dichev@huawei.com>
2024-10-16 15:30:14 +0200

    Dramatic improvement in test generation time. Do not use anymore bash processes which grep each file multiple times, but internal regex for strings populated from files. Also, replace again gtest_add_tests with gtest_discover_tests, and make it work by fixing a bug in the test launcher which was capturing output

--- test_launcher.py
+++ test_launcher.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import argparse
 import subprocess
 import sys
@@ -11,10 +12,10 @@ parser.add_argument("-t", "--lpf_probe_timer", type=float)
 parser.add_argument("-R", "--expected_return_code", type=int)
 parser.add_argument( 'cmd', nargs=argparse.REMAINDER )
 args = parser.parse_args()
+
 # This is only for passing Gtest info to CMake
-if args.cmd[1] == "--gtest_list_tests":
-    run_cmd = [args.cmd[0], args.cmd[1]]
-    cmd = subprocess.run( run_cmd, capture_output=True)
+if args.cmd[-1] == '--gtest_list_tests':
+    cmd = subprocess.run( args.cmd)
     sys.exit(cmd.returncode)
 # Actual use of our launcher
 else:
@@ -25,7 +26,7 @@ else:
             run_cmd = [args.parallel_launcher, '-engine', args.engine, '-n', str(i)] + args.cmd
         print("Run command: ")
         print(run_cmd)
-        cmd = subprocess.run( run_cmd, capture_output=True)
+        cmd = subprocess.run( run_cmd)
         print("Test returned code = " + str(cmd.returncode))
         retcode = cmd.returncode
         if (retcode != args.expected_return_code):
