6e97edb304572c9cc76c6d3d23af5013546b7de8
Kiril Dichev <kiril.dichev@huawei.com>
2024-12-20 09:08:28 +0100

    Replace hard-coded token with PAC read from env of Gitlab runner user

--- .build-tools/reframe/get_and_build.sh
+++ .build-tools/reframe/get_and_build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-rm -rf /storage/users/gitlab-runner/lpf_repo
-git clone --branch ${CI_COMMIT_REF_NAME} https://oath2:glpat-xvYANSkTDdET28F9jBxb@gitlab.huaweirc.ch/zrc-von-neumann-lab/spatial-computing/lpf/ /storage/users/gitlab-runner/lpf_repo
-pushd /storage/users/gitlab-runner/lpf_repo
+rm -rf /storage/users/${USER}/lpf_repo
+echo "CI_JOB_TOKEN: ${CI_JOB_TOKEN}"
+TOKEN=$(cat $HOME/.PERSONAL_TOKEN)
+echo "GITLAB_RUNNER_PERSONAL_TOKEN: ${TOKEN}"
+git clone --branch ${CI_COMMIT_REF_NAME} https://oath2:${TOKEN}@gitlab.huaweirc.ch/zrc-von-neumann-lab/spatial-computing/lpf/ /storage/users/${USER}/lpf_repo
+pushd /storage/users/${USER}/lpf_repo
 mkdir build
 pushd build
-../bootstrap.sh --functests; make -j32
+../bootstrap.sh --functests=i-agree-with-googletest-license; make -j32
 make -j32
+
