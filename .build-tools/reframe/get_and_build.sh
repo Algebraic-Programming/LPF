bd2a0977cafd4e1477908f571f574ea3dcbe18d9
Kiril Dichev <kiril.dichev@huawei.com>
2025-03-19 14:38:40 +0100

    Bring in gitlab ci and reframe config. Probably needs fixing to run the pipeline

---
+++ .build-tools/reframe/get_and_build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm -rf /storage/users/gitlab-runner/lpf_repo
+git clone --branch ${CI_COMMIT_REF_NAME} https://oath2:glpat-yqiQ3S1Emax8EoN91ycU@gitlab.huaweirc.ch/zrc-von-neumann-lab/spatial-computing/lpf/ /storage/users/gitlab-runner/lpf_repo
+pushd /storage/users/gitlab-runner/lpf_repo
+mkdir build
+pushd build
+../bootstrap.sh --functests; make -j32
+make -j32
---
+++ .build-tools/reframe/lpf_tests.py
@@ -0,0 +1,18 @@
+import reframe as rfm
+import reframe.utility.sanity as sn
+import os
+
+@rfm.simple_test
+class LPFFuncTests(rfm.RunOnlyRegressionTest):
+    def __init__(self):
+        self.maintainers = ['Kiril Dichev']
+        self.num_tasks = 64
+        self.num_cpus_per_task = 1
+        self.sourcesdir = '.'
+        self.prerun_cmds = ['source get_and_build.sh']
+        self.valid_systems = ['BZ:arm-sequential']
+        self.valid_prog_environs = ['*']
+        self.executable = 'ctest'
+        self.executable_opts = ['-E','"pthread|hybrid"', '--test-dir', '/storage/users/gitlab-runner/lpf_repo/build']
+        self.sanity_patterns = sn.assert_found('Tests', self.stdout)
+
